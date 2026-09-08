014def2babbee42772c01d2d0f1294cedf73e742
Sreeja Theegala <sreejatheegala@gmail.com>
2026-09-08 11:51:05 +0530

    Updated website content and fixed styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WHATSAPP_NUMBER = "919876543210";
+const WHATSAPP_NUMBER = "919182089453";
 
 function whatsappLink(message) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
